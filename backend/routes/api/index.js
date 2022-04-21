237bb72fbbe056d2dc67c71d06fbe3212c3687d2
justin chau <jchau623@gmail.com>
2022-04-21 17:51:08 -0400

    bookings

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,13 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const commentsRouter = require('./comments.js')
+// const bookingsRouter = require('./bookings.js');
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/comments', commentsRouter);
+// router.use('/bookings', bookingsRouter);
 
 //POST test route
 // router.post('/test', (req, res) => {
